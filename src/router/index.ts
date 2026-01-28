0ba0cf6378df62e54448aebfc8801869946a4c63
mingyiwang <mingyiwang@tencent.com>
2026-01-28 17:13:08 +0100

    fix: 修改路由模式为 hash 模式以支持静态部署

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import DailyLearn from '../views/DailyLearn.vue';
 import Review from '../views/Review.vue';
 import History from '../views/History.vue';
@@ -13,7 +13,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
