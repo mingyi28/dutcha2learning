7358154b50affe6186a89773932724ba8403da30
mingyiwang <mingyiwang@tencent.com>
2026-01-28 17:40:23 +0100

    Fix base path to '/' for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/dutcha2learning/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
