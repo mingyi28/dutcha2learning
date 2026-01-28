4c6fe31f9d1c8224056ec936e774fc012345f6e4
mingyiwang <mingyiwang@tencent.com>
2026-01-28 17:38:22 +0100

    Fix build assets for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/dutcha2learning/', // 替换为你的 GitHub 仓库名
+    base: '/dutcha2learning/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
